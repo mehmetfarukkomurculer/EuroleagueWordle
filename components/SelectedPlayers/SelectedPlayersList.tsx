ecf26e8be7dd0315ed72b24d0764bf5bd548bb99
mehmetfarukkomurculer <mfkomurculer@gmail.com>
2024-02-22 16:14:24 +0300

    redesigned the correct answer container & resolved the issue related to scrolling to last Element in the results list automatically

--- components/SelectedPlayers/SelectedPlayersList.tsx
+++ components/SelectedPlayers/SelectedPlayersList.tsx
@@ -1,13 +1,25 @@
 import { FlatList, StyleSheet } from "react-native";
 import { useAppSelector } from "../../redux/hooks";
 import SelectedPlayerListItem from "./SelectedPlayerListItem";
+import { useRef } from "react";
 
 const SelectedPlayersList = () => {
   const userChoices = useAppSelector((state) => state.userChoices.userChoices);
+  const ref = useRef<any>();
 
+  const scrollToIndex = () => {
+    if (userChoices.length > 0) {
+      ref.current.scrollToIndex({ animated: true, index: userChoices.length -1 });
+    }
+  };
+
+  const handleContentSizeChange = () => {
+    setTimeout(() => scrollToIndex(), 200);
+  };
   return (
     userChoices && (
       <FlatList
+        ref={ref}
         style={styles.results}
         data={userChoices}
         keyExtractor={(item) => item.id.toString() + Math.random() * 100}
@@ -23,6 +35,12 @@ const SelectedPlayersList = () => {
             playerName={item.item.name}
           />
         )}
+        getItemLayout={(userChoices, index) => ({
+          length: 150,
+          offset: 150 * index,
+          index,
+        })}
+        onContentSizeChange={handleContentSizeChange}
       />
     )
   );
